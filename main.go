4ea8ef42fc8d085c7cc95d112f948c95e40c3a30
Gregory <its.crowd@gmail.com>
2019-02-06 17:06:26 +0300

    fix

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	_ "github.com/micro/go-plugins/broker/nats"
 	"github.com/micro/go-plugins/micro/cors"
 	"github.com/micro/micro/cmd"
@@ -9,5 +11,6 @@ import (
 
 func main() {
 	plugin.Register(cors.NewPlugin())
+	fmt.Println(cors.NewPlugin())
 	cmd.Init()
 }
